821839c6956dd5022bc5681aab01d6f461b4df12
Mishenko Matwey <matweymishenko@gmail.com>
2021-02-24 23:08:43 +0300

    hello world

---
+++ app/src/main/java/com/example/adfmp1h21_read/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.adfmp1h21_read
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
